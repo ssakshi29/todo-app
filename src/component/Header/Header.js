5525fadc8b3bba3cbb4cfb84dfafceb0dec86751
Sakshi Singh <sakshisingh291999@gmail.com>
2022-05-27 17:36:38 +0530

    initial commit

---
+++ src/component/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <>
+
+            <div className='header'>
+                <h3>Todo List-App</h3>
+            </div>
+    </>
+  )
+}
+
+export default Header
